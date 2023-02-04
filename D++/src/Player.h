ab3f23c01920f00f009a7e80dc7f4761f3d0cb8b
AndreeaElek <andreea.e20@gmail.com>
2023-02-04 21:58:30 +0200

    added classes and files

---
+++ D++/src/Player.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Player
+{
+public:
+    Player();
+    ~Player();
+
+private:
+    std::string name;
+
+};
+
+#endif
